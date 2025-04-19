4f59396a5393866f4ce8a881ced914695e458f68
Raflyromeo <raflyromeonasution07@gmail.com>
2025-04-20 01:00:31 +0700

    update

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -45,10 +45,9 @@ export const useAuthStore = create((set) => ({
 		try {
 			const response = await axios.get("/api/v1/auth/authCheck");
 			set({ user: response.data.user, isCheckingAuth: false });
-		} catch (error) {
+		} catch {
 			set({ isCheckingAuth: false, user: null });
-			console.error(error); // Log the error for debugging
-			// Optional: toast.error(error.response.data.message || "An error occurred");
+			// You can choose to remove error handling altogether if not needed
 		}
 	},	
 }));
